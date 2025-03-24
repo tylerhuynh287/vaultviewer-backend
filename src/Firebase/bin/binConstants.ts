e7bea981d033a5d10bb21de5cd237566da3c1084
Tyler Huynh <tylerhuynh287@gmail.com>
2025-03-24 14:29:41 -0400

    Forgot to save minor changes in binsConstants.ts

--- src/Firebase/bin/binConstants.ts
+++ src/Firebase/bin/binConstants.ts
@@ -5,12 +5,13 @@ export enum BinCollections {
 
   export enum BinSuccessMessages {
     BIN_SUCCESS_UPDATE = "Success: Bin was updated",
-    BIN_SUCCESS_DELETE = "Success: All successfully delete"
+    BIN_SUCCESS_DELETE = "Success: Bin and all items successfully deleted"
   }
 
   export enum BinErrorMessages {
-    MISSING_BINS = "Error: No Bins Found",
+    MISSING_BIN = "Error: Bin not found",
+    MISSING_BIN_ID = "Error: Missing Bin ID",
     GENERIC_BIN_ERROR = "Error: Unable to Fetch Bins",
     UPDATE_BIN_ERROR  = "Error: Unable to update Bin",
-    DELETE_BIN_ERROR = "Error: Unable to delete bin"
+    DELETE_BIN_ERROR = "Error: Unable to delete Bin"
   }
