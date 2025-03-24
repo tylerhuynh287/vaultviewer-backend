c3dd31d44b9a83c5a2512b95b72589f0c76edea3
Jethro Talavera <jet@Jethros-MacBook-Air.local>
2025-03-24 08:16:10 -0400

    clean-up

---
+++ src/Firebase/bin/binConstants.ts
@@ -0,0 +1,16 @@
+export enum BinCollections {
+    USERS = "users",
+  }
+   
+
+  export enum BinSuccessMessages {
+    BIN_SUCCESS_UPDATE = "Success: Bin was updated",
+    BIN_SUCCESS_DELETE = "Success: All successfully delete"
+  }
+
+  export enum BinErrorMessages {
+    MISSING_BINS = "Error: No Bins Found",
+    GENERIC_BIN_ERROR = "Error: Unable to Fetch Bins",
+    UPDATE_BIN_ERROR  = "Error: Unable to update Bin",
+    DELETE_BIN_ERROR = "Error: Unable to delete bin"
+  }
